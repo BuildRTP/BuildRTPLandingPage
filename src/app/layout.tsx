e778d71453ec3815f337294c43efe5e64bc31b5d
Arnav Chauhan <arnav216@gmail.com>
2025-02-16 03:33:44 -0500

    eslint bs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Montserrat } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const montserrat = Montserrat({
   subsets: ['latin'],
