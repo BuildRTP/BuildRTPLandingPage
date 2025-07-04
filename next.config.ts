ec80138f9d4caafc90f9dadaba42ab2a7da187b0
Arnav Chauhan <67533986+ImaginaryCeiling@users.noreply.github.com>
2025-07-04 02:56:11 -0400

    Update next.config.ts with /event-proposal route

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,11 @@ const nextConfig: NextConfig = {
         destination: 'https://scrapyard.hackclub.com/rtp', 
         permanent: false, 
       },
+      {
+        source: '/event-proposal',
+        destination: "https://docs.google.com/document/d/1cF10m0gzHcqdn1xIsuhV4C7XlR2LozKS-uXvVP9GrtE/edit?tab=t.9yeglrig2w0r#heading=h.3j35vko49jjl",
+        permanent: false,
+      },
     ];
   },
 };
