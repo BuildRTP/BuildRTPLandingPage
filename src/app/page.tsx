22212805f5f868393e25979f688b5365c64a9941
Arnav Chauhan <67533986+ImaginaryCeiling@users.noreply.github.com>
2025-08-05 19:44:11 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <EventCard
           eventType="PITCH COMPETITION"
           title="LaunchUP"
-          date="Saturday, August 10th - 12:30PM to 6PM"
+          date="Sunday, August 10th - 12:30PM to 6PM"
           location="5310 S Alston Ave. STE 200, Durham, NC 27713"
           description="Join us for a pitch competition with thousands in prize money! Come in with any ideas, with a team of up to 4 people and get feedback from judges with decades of experience!"
           signupUrl="/launchup"
