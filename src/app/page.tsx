c2158d4449e999276e927880c6b4919695ff3f09
Arnav Chauhan <arnav216@gmail.com>
2025-09-29 09:29:07 -0400

    name change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
         <EventCard
           eventType="STARTUP ACCELERATOR"
-          title="LaunchUP Accelerator"
+          title="LaunchUp Accelerator"
           date="Nov 2nd - Dec 14th"
           location="5310 S Alston Ave. STE 200, Durham, NC 27713"
           description="In collaboration with LaunchChapelHill and Innovate Carolina at UNC, we are offering a 6 week accelerator program for high school startups in the RTP area. Join us to get feedback from experts in your industry area, guidance from mentors that have been in your shoes, and opportunities to pitch to investors."
